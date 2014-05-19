3b9656a01f6bec40bc8bc2d4d606210325447fd1
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-19 19:48:55 +0300

    add line parsing support for consumption parser

--- consumption_parser.rb
+++ consumption_parser.rb
@@ -1,3 +1,11 @@
 class ConsumptionParser
 
+  def parse_line(line)
+    line_splits = line.split(' ')
+
+    result = {
+      :kw_consumed => line_splits[2][0..-3].to_i,
+      :hour_of_month => line_splits[0].to_i
+    }
+  end
 end
--- test/consumption_parser_spec.rb
+++ test/consumption_parser_spec.rb
@@ -3,4 +3,17 @@ require './consumption_parser'
 describe ConsumptionParser do
   let(:parser) {ConsumptionParser.new}
 
+  describe '#parse_line' do
+    it 'should parse kW consumed' do
+      parsed = parser.parse_line('32 - 6kW (hour #32 is January 2, Monday, 07:00 - 08:00 - DAY)')
+
+      parsed[:kw_consumed].should eq(6);
+    end
+
+    it 'should parse the hour of month' do
+      parsed = parser.parse_line('32 - 6kW (hour #32 is January 2, Monday, 07:00 - 08:00 - DAY)')
+
+      parsed[:hour_of_month].should eq(32);
+    end
+  end
 end
