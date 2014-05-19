c2ccece1b19f8b63a1363847e82d17cf3d773a66
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-19 20:59:36 +0300

    Update DateUtil to have class methods instead of instance ones

--- date_util.rb
+++ date_util.rb
@@ -1,19 +1,21 @@
 class DateUtil
 
-  def is_day_hour(time)
-    hour_in_day_range(time) && !is_weekend(time)
-  end
-
-  private
-  def hour_in_day_range(time)
-    if time.dst?
-      time.hour >= 8
-    else
-      time.hour >= 7 && time.hour < 23
+  class << self
+    def is_day_hour(time)
+      hour_in_day_range(time) && !is_weekend(time)
     end
-  end
 
-  def is_weekend(time)
-    time.sunday? || time.saturday?
+    private
+    def hour_in_day_range(time)
+      if time.dst?
+        time.hour >= 8
+      else
+        time.hour >= 7 && time.hour < 23
+      end
+    end
+
+    def is_weekend(time)
+      time.sunday? || time.saturday?
+    end
   end
 end
--- spec/date_util_spec.rb
+++ spec/date_util_spec.rb
@@ -2,43 +2,41 @@ require './date_util'
 require 'date'
 
 describe DateUtil do
-  let(:date_util) {DateUtil.new}
-
   describe '#is_day_hour' do
     it 'should return false for night hours' do
       (0..6).each do |hour|
-        date_util.is_day_hour(Time.new(2012, 1, 2, hour)).should be_false
+        DateUtil.is_day_hour(Time.new(2012, 1, 2, hour)).should be_false
       end
-      date_util.is_day_hour(Time.new(2012, 1, 2, 23)).should be_false
+      DateUtil.is_day_hour(Time.new(2012, 1, 2, 23)).should be_false
     end
 
     it 'should return true for day hours' do
       (7..22).each do |hour|
-        date_util.is_day_hour(Time.new(2012, 1, 2, hour)).should be_true
+        DateUtil.is_day_hour(Time.new(2012, 1, 2, hour)).should be_true
       end
     end
 
     it 'should return false for night hours during daylight saving' do
       (0..7).each do |hour|
-        date_util.is_day_hour(Time.new(2012, 6, 4, hour)).should be_false
+        DateUtil.is_day_hour(Time.new(2012, 6, 4, hour)).should be_false
       end
     end
 
     it 'should return true for day hours during daylight saving' do
       (8..23).each do |hour|
-        date_util.is_day_hour(Time.new(2012, 6, 4, hour)).should be_true
+        DateUtil.is_day_hour(Time.new(2012, 6, 4, hour)).should be_true
       end
     end
 
     it 'should return false for all hours during sundays' do
       (0..23).each do |hour|
-        date_util.is_day_hour(Time.new(2012, 1, 1, hour)).should be_false
+        DateUtil.is_day_hour(Time.new(2012, 1, 1, hour)).should be_false
       end
     end
 
     it 'should return false for all hours during saturdays' do
       (0..23).each do |hour|
-        date_util.is_day_hour(Time.new(2012, 1, 7, hour)).should be_false
+        DateUtil.is_day_hour(Time.new(2012, 1, 7, hour)).should be_false
       end
     end
   end
