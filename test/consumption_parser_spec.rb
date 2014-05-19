c2e7c9dd371e4bf85d9f043288fe12a8d2c90b9c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-19 19:54:23 +0300

    clean the line parser test to use context
    keeping it DRY

--- test/consumption_parser_spec.rb
+++ test/consumption_parser_spec.rb
@@ -4,16 +4,16 @@ describe ConsumptionParser do
   let(:parser) {ConsumptionParser.new}
 
   describe '#parse_line' do
-    it 'should parse kW consumed' do
-      parsed = parser.parse_line('32 - 6kW (hour #32 is January 2, Monday, 07:00 - 08:00 - DAY)')
+    context 'with parsed sample' do
+      let(:parsed) {parser.parse_line('32 - 6kW (hour #32 is January 2, Monday, 07:00 - 08:00 - DAY)')}
 
-      parsed[:kw_consumed].should eq(6);
-    end
+      it 'should parse kW consumed' do
+        parsed[:kw_consumed].should eq(6);
+      end
 
-    it 'should parse the hour of month' do
-      parsed = parser.parse_line('32 - 6kW (hour #32 is January 2, Monday, 07:00 - 08:00 - DAY)')
-
-      parsed[:hour_of_month].should eq(32);
+      it 'should parse the hour of month' do
+        parsed[:hour_of_month].should eq(32);
+      end
     end
   end
 end
